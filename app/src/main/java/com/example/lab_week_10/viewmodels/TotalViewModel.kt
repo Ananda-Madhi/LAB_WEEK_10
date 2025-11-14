88bbabca5c3dad2b238700dd4a82cbd20a3e7a15
Ananda-Madhi <147123700+Ananda-Madhi@users.noreply.github.com>
2025-11-14 17:54:43 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation By Ananda Christopher Gunawan

---
+++ app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.lab_week_10.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
+
+class TotalViewModel : ViewModel() {
+
+    private val _total = MutableLiveData(0)
+    val total: LiveData<Int> get() = _total
+
+    fun incrementTotal() {
+        _total.value = (_total.value ?: 0) + 1
+    }
+}
